c1edeff99f15b6edf61cb1cb73838c2d89d7871b
Garvit-Ag <168018359+Garvit-Ag@users.noreply.github.com>
2024-07-08 12:30:58 +0530

    Create ExampleUnitTest.kt

---
+++ ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package com.av.avmessenger
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @org.junit.Test
+    fun addition_isCorrect() {
+        org.junit.Assert.assertEquals(4, (2 + 2).toLong())
+    }
+}
